39d0df94abc7e098f607fb71c8570a6de889e923
tmlaes <wangzezheng1991@163.com>
2022-09-02 11:35:36 +0800

    init
    init

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+module.exports = {
+	mode: "production",
+	entry: {
+		index: path.resolve(__dirname, "src", "index.js")
+	},
+	output: {
+		path: path.resolve(__dirname, "build")
+	},
+	plugins: [
+		// 清除dist文件夹
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src", "index.html")
+		}),
+		new webpack.ProvidePlugin({
+			process: 'process/browser',
+			Buffer: ['buffer', 'Buffer']
+		})
+	],
+	devServer: {
+		hot: true,
+		port: 8080,
+		https:true,
+		static: {
+			directory: path.join(__dirname, "/src/static")
+		},
+		
+		proxy:{
+			/**
+			 * target : 表示的是代理到的目标地址
+　　			 * pathRewrite: 默认情况下，我们的 /api-hy 也会被写到 RUL 中，如果希望删除，可以使用 pathRewrite 
+　　			 * secure ：默认情况下不接受转发到 https 的服务器上的，如果希望支持，可以设置为 false
+　　			 * changeOrigin: 它是表示是否更新代理后请求的 headers 中的 host 地址
+			 */
+			'/api':{
+			    target:'http://localhost:8081',
+			    pathRewrite:{'/api':''},
+				secure:false,
+				changeOrigin:true
+			}
+		}
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ["style-loader", "css-loader"],
+			exclude: /node_modules/
+		}]
+	},
+	resolve: {
+		fallback: {
+			"stream": require.resolve("stream-browserify"),
+			"crypto": require.resolve("crypto-browserify"),
+			"assert": require.resolve("assert/"),
+			"http": require.resolve("stream-http"),
+			"https": require.resolve("https-browserify")
+		}
+	}
+}
